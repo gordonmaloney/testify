32bd5255de5919fcf383b28ece775c89bfc8f8d7
gordonmaloney <gordonmaloney@gmail.com>
2025-11-26 12:36:05 +0100

    postcode

--- src/MiniMap.jsx
+++ src/MiniMap.jsx
@@ -3,15 +3,8 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
-/*
-// Fix default icon issue with Leaflet in React
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-	iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-	iconUrl: require("leaflet/dist/images/marker-icon.png"),
-	shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-});
-*/
+
+
 
 const MiniMap = ({ postcode }) => {
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import "./leaflet.config"; // must be before using <MapContainer> etc.
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
